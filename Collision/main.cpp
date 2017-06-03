c7faa28f713eb35c9eeced40b029e1d88a78406a
kilian3309 <kiki.jugie@gmail.com>
2017-06-03 22:47:48 +0200

    ''

--- Collision/main.cpp
+++ Collision/main.cpp
@@ -226,7 +226,7 @@ void CreateLoadingScreen(HINSTANCE hInst = (HINSTANCE)nullptr, int maxRange=15) 
 
 	cyVScroll = GetSystemMetrics(SM_CYVSCROLL);
 
-	hwndPB = CreateWindowEx(1, PROGRESS_CLASS, (LPCTSTR)NULL, WS_CHILD | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 145, 70, hwndLS, 0, hInst, NULL);
+	hwndPB = CreateWindowEx(PBS_MARQUEE, PROGRESS_CLASS, (LPCTSTR)NULL, WS_CHILD | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 145, 70, hwndLS, 0, hInst, NULL);
 	
 	
 	
