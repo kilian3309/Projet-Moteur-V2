092e6808f34a6767d585014df6b035f6eb5a59c3
kilian3309 <kiki.jugie@gmail.com>
2017-06-03 15:59:09 +0200

    ''

--- Collision/main.cpp
+++ Collision/main.cpp
@@ -250,7 +250,7 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 			TEXT("Collision"), MB_OK | MB_ICONEXCLAMATION);
 		return -1;
 	}
-
+	MessageBox(NULL, TEXT("h"), TEXT("hj"), MB_OK);
 
 
 
