5fce55e70eff2565d803f07f3a1f83fc51e06182
kilian3309 <kiki.jugie@gmail.com>
2017-06-10 10:40:30 +0200

    ''

--- Collision/LoadingScreen.h
+++ Collision/LoadingScreen.h
@@ -4,6 +4,29 @@
 #include <Uxtheme.h>
 #pragma comment(lib, "UxTheme.lib")
 
+/*
+	Système de compteur compil-time
+*/
+#define COUNTER_READ_CRUMB(TAG, RANK, ACC) counter_crumb(TAG(), constant_index<RANK>(), constant_index<ACC>())
+//Lecture du compteur
+#define COUNTER_READ( TAG ) COUNTER_READ_CRUMB(TAG, 1, COUNTER_READ_CRUMB(TAG, 2, COUNTER_READ_CRUMB(TAG, 4, COUNTER_READ_CRUMB(TAG, 8, \
+		COUNTER_READ_CRUMB( TAG, 16, COUNTER_READ_CRUMB(TAG, 32, COUNTER_READ_CRUMB(TAG, 64, COUNTER_READ_CRUMB( TAG, 128, 0))))))))
+
+//Incrémentation du compteur
+#define COUNTER_INC(TAG) \
+constexpr \
+constant_index<COUNTER_READ(TAG)+1> \
+counter_crumb(TAG, constant_index<(COUNTER_READ(TAG)+1)&~COUNTER_READ(TAG)>, constant_index<(COUNTER_READ(TAG)+1)&COUNTER_READ(TAG)>) {return {};}
+#define COUNTER_LINK_NAMESPACE(NS) using NS::counter_crumb;
+
+
+template<std::size_t n>
+struct constant_index : std::integral_constant< std::size_t, n > {};
+
+template<typename id, std::size_t rank, std::size_t acc>
+constexpr constant_index<acc> counter_crumb(id, constant_index<rank>, constant_index<acc>) { return {}; } 
+
+
 //Variables de la fenêtre de chargement
 HWND hwndPB; //Handle vers la progress bar
 HWND hwndLS; //Handle vers la fenêtre de chargement
